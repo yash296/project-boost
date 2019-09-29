472e8d5445d5b9a562b5873220886d0e524bc3d9
yash296 <wanvari.yash@gmail.com>
2019-09-29 12:39:21 +0530

    added move left and right with rocket audio

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = GetComponent<Rigidbody>();        
+        rigidBody = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -23,14 +25,20 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             rigidBody.AddRelativeForce(Vector3.up);
+            if(!audioSource.isPlaying) audioSource.Play();
+
+        }
+        else
+        {
+            audioSource.Stop();
         }
         if (Input.GetKey(KeyCode.A))
         {
-            print("Left");
+            transform.Rotate(Vector3.forward);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            print("Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
