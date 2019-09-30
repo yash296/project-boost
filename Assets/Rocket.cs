75343a22ca351210923857f4852415ba3dfd3dc0
yash296 <wanvari.yash@gmail.com>
2019-09-30 09:26:22 +0530

    in progress

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    [SerializeField] float rcsThrust = 250f;
+    [SerializeField] float thrustValue = 800f;
     Rigidbody rigidBody;
     AudioSource audioSource;
     // Start is called before the first frame update
@@ -17,28 +19,39 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
-
-    private void ProcessInput()
+    private void Thrust()
     {
+        float thrustThisFrame = thrustValue * Time.deltaTime;
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
-            if(!audioSource.isPlaying) audioSource.Play();
+            rigidBody.AddRelativeForce(Vector3.up * thrustThisFrame);
+            if (!audioSource.isPlaying) audioSource.Play();
 
         }
         else
         {
             audioSource.Stop();
         }
+    }
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; //take manual control of roation
+        float roatationThisFrame = rcsThrust * Time.deltaTime;
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            
+            transform.Rotate(Vector3.forward * roatationThisFrame);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * roatationThisFrame);
         }
+        rigidBody.freezeRotation = false; //resume pysicys control
+
     }
+
+
 }
