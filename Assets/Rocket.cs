a551c8f385ed63b5c153c16b958b8d5eedeb2a23
yash296 <wanvari.yash@gmail.com>
2019-10-10 09:34:57 +0530

    added particles for rocket, success and death sequence

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -11,6 +11,9 @@ public class Rocket : MonoBehaviour
     [SerializeField] AudioClip mainEngine;
     [SerializeField] AudioClip success;
     [SerializeField] AudioClip death;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem deathParticles;
     Rigidbody rigidBody;
     AudioSource audioSource;
     enum State { Alive, Dying, Transcending };
@@ -55,6 +58,7 @@ public class Rocket : MonoBehaviour
         state = State.Dying;
         audioSource.Stop();
         audioSource.PlayOneShot(death);
+        deathParticles.Play();
         Invoke("LoadFirstLevel", 1f);
     }
 
@@ -63,32 +67,43 @@ public class Rocket : MonoBehaviour
         state = State.Transcending;
         audioSource.Stop();
         audioSource.PlayOneShot(success);
+        successParticles.Play();
         Invoke("LoadNextLevel", 1f); //parameterise time
     }
 
     private  void LoadFirstLevel()
     {
         SceneManager.LoadScene(0);
+        deathParticles.Stop();
     }
 
     private void LoadNextLevel()
     {
         SceneManager.LoadScene(1);
+        successParticles.Stop();
     }
 
     private void RepondToThrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
-            if (!audioSource.isPlaying) { audioSource.PlayOneShot(mainEngine); }
+            ApplyThrust();
 
         }
         else
         {
             audioSource.Stop();
+            mainEngineParticles.Stop();
         }
     }
+
+    private void ApplyThrust()
+    {
+        rigidBody.AddRelativeForce(Vector3.up * mainThrust);
+        if (!audioSource.isPlaying) { audioSource.PlayOneShot(mainEngine); }
+        mainEngineParticles.Play();
+    }
+
     private void RespondToRotate()
     {
         rigidBody.freezeRotation = true; //take manual control of roation
